f8954322b49c8d94a8d8f45abf9df5a897c210ba
Gajendran <billgajen@gmail.com>
2019-02-25 13:58:32 +0000

    Inital push 3

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const validateWebhook = require('./server/webhooks');
 
 const bodyParser = require('koa-bodyparser');
 
-const port = parseInt(process.env.PORT, 10);
+const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
