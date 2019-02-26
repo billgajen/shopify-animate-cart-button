e1dfd6e52699e76df94daebf5fd6ac56dc1e05de
Gajendran <billgajen@gmail.com>
2019-02-26 13:14:21 +0000

    Inital push 3

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const Koa = require('koa');
-const next = require('next');
-require('isomorphic-fetch');
 
 const bodyParser = require('koa-bodyparser');
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
 
 const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY, TUNNEL_URL } = process.env;
 
